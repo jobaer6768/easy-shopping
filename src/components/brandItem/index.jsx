296edde5b51f0b222fe8655161ff6291cb9b61e1
jobaer6768 <jobaer9898@gmail.com>
2025-03-09 02:13:02 +0600

    updated brand-page and brand-item also made it responsive

--- src/components/brandItem/index.jsx
+++ src/components/brandItem/index.jsx
@@ -5,13 +5,11 @@ import Title from "../../ui/title";
 
 const BrandItem = ({ name }) => {
   return (
-    <div className="px-4 sm:px-10 md:px-24 lg:px-[346px]">
-      <div className="flex flex-col gap-4 cursor-pointer hover:bg-[#f4f4f4] transition-all duration-200 ease-in-out rounded-md">
-        <Title className="text-sm sm:text-base font-light mt-2 sm:mt-3 lg:pl-4">
-          {name}
-        </Title>
-        <Divider />
-      </div>
+    <div className="flex flex-col gap-4 cursor-pointer hover:bg-[#f4f4f4] transition-all duration-200 ease-in-out rounded-md">
+      <Title className="text-sm sm:text-base font-light mt-2 pl-2 sm:mt-3 lg:pl-4">
+        {name}
+      </Title>
+      <Divider />
     </div>
   );
 };
--- src/pages/brand-page/index.jsx
+++ src/pages/brand-page/index.jsx
@@ -1,13 +1,65 @@
 import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
 
 import BrandItem from "../../components/brandItem";
+import Divider from "../../ui/divider";
+import Title from "../../ui/title";
 
 const BrandPage = ({ brands }) => {
+  const [groupedBrands, setGroupedBrands] = useState({});
+
+  useEffect(() => {
+    const sortedBrands = [...brands].sort((a, b) =>
+      a.name.toLowerCase().localeCompare(b.name.toLowerCase())
+    );
+
+    const grouped = sortedBrands.reduce((acc, brand) => {
+      const firstLetter = brand.name[0].toUpperCase();
+      if (!acc[firstLetter]) acc[firstLetter] = [];
+
+      acc[firstLetter].push(brand);
+
+      return acc;
+    }, {});
+
+    setGroupedBrands(grouped);
+  }, [brands]);
+
   return (
-    <div>
-      {brands?.map((brand) => {
-        return <BrandItem key={brand.id} name={brand.name} />;
-      })}
+    <div className="max-w-5xl mx-auto p-4">
+      {/* heading */}
+      <Title className="text-center text-xl md:text-2xl font-bold">
+        A-Z Men&apos;s Brands
+      </Title>
+      <Divider className="my-2 mb-4" />
+
+      {/* A-Z Navigation */}
+      <div className="text-[12px] md:text-sm flex justify-center space-x-2 md:space-x-6 mb-6">
+        {Object.keys(groupedBrands).map((letter) => (
+          <a
+            key={letter}
+            href={`#${letter}`}
+            className="hover:underline hover:text-base cursor-pointer"
+          >
+            {letter}
+          </a>
+        ))}
+      </div>
+
+      {/* Brands List */}
+      {Object.keys(groupedBrands).map((letter) => (
+        <div key={letter} id={letter} className="mb-8">
+          {/* Section Header */}
+          <div className="bg-gray-100 text-lg font-medium py-2 px-4">
+            {letter}
+          </div>
+
+          {/* Brand Items */}
+          {groupedBrands[letter].map((brand) => (
+            <BrandItem key={brand.id} name={brand.name} />
+          ))}
+        </div>
+      ))}
     </div>
   );
 };
@@ -17,9 +69,6 @@ BrandPage.propTypes = {
     PropTypes.shape({
       id: PropTypes.number.isRequired,
       name: PropTypes.string.isRequired,
-      logo: PropTypes.string.isRequired,
-      description: PropTypes.string,
-      date_added: PropTypes.string.isRequired,
     })
   ).isRequired,
 };
